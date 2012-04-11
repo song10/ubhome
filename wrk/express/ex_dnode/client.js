e8454e3575fc586adafebcc87953a90910cef108
song10 <song10@gmail.com>
2012-04-11 22:35:53 +0800

    nodejs: update dnode demo

--- wrk/express/ex_dnode/client.js
+++ wrk/express/ex_dnode/client.js
@@ -1,6 +1,6 @@
 var dnode = require('dnode');
 
-dnode.connect(5050, 'localhost', function (remote) {
+dnode.connect(5050, 'miniub', function (remote) {
     remote.mul(10, 20, function (n) {
         console.log('10 * 20 = ' + n);
     });
