32b71de8e2132e5c5fd898d807e4ca4a1fa2e7aa
song10 <song10@gmail.com>
2012-04-19 20:31:07 +0800

    xman: add oauth for twitter, google and facebook

--- wrk/express/xman/app.js
+++ wrk/express/xman/app.js
@@ -4,9 +4,75 @@
  */
 
 var express = require('express')
+  , params = require('express-params')
   , routes = require('./routes');
 
 var app = module.exports = express.createServer();
+params.extend(app);
+
+app.param('fbcb', /^(\w+)\.\.(\w+)?$/);
+
+// everyauth
+
+var everyauth = require('everyauth');
+var conf = require('./config.json');
+everyauth.twitter
+  .consumerKey(conf.twit.consumerKey)
+  .consumerSecret(conf.twit.consumerSecret)
+  .findOrCreateUser(function(session, token, secret, user) {
+    return this.Promise().fulfill(user);
+  })
+  .redirectPath('/');
+
+everyauth.facebook
+  .myHostname('http://song10.mooo.com:8080')
+//  .accessTokenHttpMethod('get')
+//  .postAccessTokenParamsVia('query')
+  .appId(conf.fb.appId)
+  .appSecret(conf.fb.appSecret)
+//  .scope('email')
+//  .moduleTimeout(30000)
+  //.handleAuthCallbackError(function (req, res) {
+    //// If a user denies your app, Facebook will redirect the user to
+    //// /auth/facebook/callback?error_reason=user_denied&error=access_denied&error_description=The+user+denied+your+request.
+    //// This configurable route handler defines how you want to respond to
+    //// that.
+    //// If you do not configure this, everyauth renders a default fallback
+    //// view notifying the user that their authentication failed and why.
+  //})
+  .findOrCreateUser(function (session, accessToken, accessTokExtra, fbUserMetadata) {
+    console.log(fbUserMetadata);
+    return this.Promise().fulfill(fbUserMetadata);
+    })
+  .moduleTimeout(-1)
+  .redirectPath('/');
+
+//console.log(everyauth.facebook.configurable());
+//console.log(everyauth.facebook.callbackPath());
+//console.log(everyauth.facebook.routes);
+
+// Google
+everyauth.google
+  .appId(conf.google.appId)
+  .appSecret(conf.google.appSecret)
+  .callbackPath('/oauth2callback')
+//  .scope('http://www.google.com/calendar/feeds/')
+//  .scope('https://www.googleapis.com/auth/userinfo.email')
+  .scope('https://www.googleapis.com/auth/userinfo.profile')
+    //.handleAuthCallbackError( function (req, res) {
+    //// If a user denies your app, Google will redirect the user to
+    //// /auth/facebook/callback?error=access_denied
+    //// This configurable route handler defines how you want to respond to
+    //// that.
+    //// If you do not configure this, everyauth renders a default fallback
+    //// view notifying the user that their authentication failed and why.
+  //})
+  .findOrCreateUser( function (session, accessToken, accessTokenExtra, googleUserMetadata) {
+    //console.log(googleUserMetadata);
+    return this.Promise().fulfill(googleUserMetadata);
+    })
+  .moduleTimeout(-1)
+  .redirectPath('/');
 
 // Configuration
 
@@ -16,8 +82,9 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
+  app.use(express.session({ secret: 'xmanagesys001' }));
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(everyauth.middleware());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -30,10 +97,104 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+// listen
+
+app.listen(8080, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+
+// sub
+function objectToString(o){
+    var parse = function(_o){
+        var a = [], t;
+        for(var p in _o){
+            if(_o.hasOwnProperty(p)){
+                t = _o[p];
+                if(t && typeof t == "object"){
+                    a[a.length]= p + ":{ " + arguments.callee(t).join(", ") + "}";
+                } else if(typeof t == "string"){
+                    a[a.length] = [ p+ ": \"" + t.toString() + "\"" ];
+                } else {
+                    a[a.length] = [ p+ ": " + t.toString()];
+                }
+            }
+        }
+        return a;
+    }
+    return "{" + parse(o).join(", ") + "}";
+}
+
+String.prototype.keyMatch = function(re, flags){
+    var is_global = false,
+        results = [],
+        keys = {},
+        native_re=null,
+        str = this,
+        tmpstr = str;
+
+    if(flags === undefined)
+        flags = "";
+
+    // find the keys inside the re, and place in mapping array {'1':'key1', '2':'key2', ...}
+    var tmpkeys = re.match(/(?!\(\?\P\<)(\w+)(?=\>)/g);
+    if(!tmpkeys){  // no keys, do a regular match
+        return str.match(re);
+    }else{
+        for(var i=0,l=tmpkeys.length; i<l; i++){
+            keys[i] = tmpkeys[i];
+        }
+    }
+
+    // remove keys from regexp leaving standard regexp
+    native_re = re.replace(/\?\P\<\w+\>/g,'');
+
+    if(flags.indexOf('g') >= 0)
+        is_global = true;
+    flags = flags.replace('g','');
+
+    native_re = RegExp(native_re, flags);
+    do{
+        // parse string
+        var tmpmatch = tmpstr.match(native_re),
+            tmpkeymatch = {},
+            tmpsubstr = "";
+
+        if(tmpmatch){
+            // get the entire string found
+            tmpsubstr = tmpmatch[0];
+            tmpkeymatch[0] = tmpsubstr;
+            // map them back out
+            for(var i=1,l=tmpmatch.length; i<l; i++){
+                tmpkeymatch[keys[i-1]] = tmpmatch[i];
+            }
+            // add to results
+            results.push(tmpkeymatch);
+            tmpstr = tmpstr.slice( (tmpstr.indexOf(tmpsubstr)+tmpsubstr.length) );
+        }else{
+            tmpstr = "";
+        }
+    } while(is_global && tmpstr.length > 0) // if global loop until end of str, else do once
+
+    return results;
+}
+
 // Routes
 
 app.get('/', routes.index);
+app.get('/login', routes.login);
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+app.get('/new', routes.new_get);
+app.post('/new', routes.new_post);
+
+// facebook workaround
+app.get('/test/:id/?$', function(req,res) {
+  res.send(objectToString(req.params));
+  console.log(req.params);
+  });
+app.get('/test2/:fbcb', function(req,res) {
+  //console.log(req.params);
+  var results = req.params.fbcb[0].keyMatch('(?P<num1>\\w+)\\.\\.(?P<num2>\\d+)', "g");
+  //console.log(results);
+  res.send(objectToString(req.params)+'<br/>'+objectToString({'fbcb': results[0]}));
+  });
+app.get('^(\/auth\/facebook\/callback\?code=.+)#_=_$', routes.fb_wa);
--- wrk/express/xman/routes/index.js
+++ wrk/express/xman/routes/index.js
@@ -3,6 +3,85 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+exports.index = function(req, res) {
+    var user;
+    if (req.session.auth) {
+      if (req.session.auth.twitter) {
+        console.log("===req.session.auth.twitter===");
+        console.log(req.session.auth.twitter);
+        user = req.session.auth.twitter.user.screen_name;
+      } else if (req.session.auth.google) {
+        console.log("===req.session.auth.google===");
+        console.log(req.session.auth.google);
+        user = req.session.auth.google.user.name;
+      }
+    }
+
+    if (! user) {
+      res.redirect('/login');
+      return;
+    }
+
+    return article.findAll(function(err, docs) {
+      return res.render('index', {
+        locals: {
+          title: 'CouchPress',
+          articles: docs,
+          user: user
+        }
+      });
+    });
+};
+
+exports.login = function(req, res){
+    var user;
+    if (req.session.auth) {
+      if (req.session.auth.twitter) {
+        user = req.session.auth.twitter.user.screen_name;
+      } else if (req.session.auth.google) {
+        //console.log(req.session.auth.google);
+        user = req.session.auth.google.user.name;
+      }
+    }
+
+    if (user) {
+      res.redirect('/');
+      return;
+    }
+
+    res.render('login', {
+      layout: false,
+      locals: {
+        title: "Login App",
+        user: user
+      }
+    });
+};
+
+exports.new_get = function(req, res) {
+    return res.render('new', {
+      locals: {
+        title: 'CouchPress / New Post'
+      }
+    });
+};
+
+// couchDB
+var Articler = require('../articler').Articler;
+var article = new Articler('http://localhost', 5984);
+
+exports.new_post = function(req, res) {
+    return article.save({
+      title: req.param('title'),
+      body: req.param('body'),
+      created_at: new Date()
+    }, function(err, docs) {
+      return res.redirect('/');
+    });
+};
+
+exports.fb_wa = function(req, res) {
+    console.log(req);
+    //res.redirect(req.param[0]);
+    res.send(req);
 };
