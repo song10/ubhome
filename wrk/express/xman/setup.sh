f076cc4dc589b067537f5b7afdd8956d18b2374c
song10 <song10@gmail.com>
2012-05-18 07:36:03 +0800

    xamn: separate

--- wrk/express/xman/setup.sh
+++
@@ -1,1 +0,0 @@
-npm install express express-params cradle everyauth stylus jade markdown
--- wrk/express/xman/tjson.js
+++
@@ -1,6 +0,0 @@
-fs = require('fs')
-require.extensions[".json"] = function (m) {
- m.exports = JSON.parse(fs.readFileSync(m.filename));
-};
-var xxx = require('./cfg.json');
-console.log(xxx.twit.consumerKey);
